3a8251e4294b3953f0dfe985d683267c2e5c54e5
Zach <Zach@Zacharys-MacBook-Air.local>
2017-08-30 14:21:51 -0500

    Q5

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv') as r:
+    emails = list()
+    csv_reader = csv.reader(r, delimiter=',')
+    header = next(csv_reader, None)
+    for row in csv_reader:
+        emails.append(row[3])
+    r.close()
+
+with open('emails.csv', 'w', newline='') as f:
+    csv_writer = csv.writer(f, delimiter='\n')
+    csv_writer.writerow(emails)
+    f.close()
+
