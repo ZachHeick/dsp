05097e01d1c6ef9b2e7ae99d0eef0c43581b34db
Zach <Zach@Zacharys-MacBook-Air.local>
2017-08-30 13:46:58 -0500

    Python Advanced Part 1 Q1

--- 05b-python_advanced.md
+++ 05b-python_advanced.md
@@ -19,7 +19,9 @@ This data is available in this file:  [faculty.csv](python/faculty.csv)
 
 #### Q1. Find how many different degrees there are, and their frequencies: Ex:  PhD, ScD, MD, MPH, BSEd, MS, JD, etc.
 
->> REPLACE THIS WITH YOUR RESPONSE
+>> ```python
+d = {'ScD': 6, 'PhD': 31, 'MD': 1, 'MPH': 2, 'BSEd': 1, 'MS': 2, 'JD': 1, 'MA': 1, '0': 1}
+```
 
 
 #### Q2. Find how many different titles there are, and their frequencies:  Ex:  Assistant Professor, Professor
--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,38 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    header = next(csv_reader, None)
+
+    degree_freq = dict()
+    titles_freq = dict()
+    emails = list()
+    email_domains = set()
+
+    for row in csv_reader:
+        degrees = [deg.replace('.', '') for deg in row[1].split()]
+        title = row[2].split()[0]
+        emails.append(row[3])
+
+        for deg in degrees:
+            if deg not in degree_freq:
+                degree_freq[deg] = 1
+            else:
+                degree_freq[deg] += 1
+
+        if title not in titles_freq:
+            titles_freq[title] = 1
+        else:
+            titles_freq[title] += 1
+
+    print(degree_freq)
+    print(titles_freq)
+    print(emails)
+
+    for email in emails:
+        email_domain = email.split('@')[-1]
+        email_domains.add(email_domain)
+
+    print(list(email_domains))
+
+
